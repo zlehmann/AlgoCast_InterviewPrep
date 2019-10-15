aa50915d1f459ede037edea565a1c063866c01ef
Zachary Lehmann <zlehmann@gmail.com>
2019-10-14 20:46:06 -0400

    cleaner solution of reverseint problem.

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -9,18 +9,22 @@
 //   reverseInt(-90) === -9
 
 function reverseInt(n) {
-  if (n === 0) {
-    return 0
-  }
+//  if (n === 0) {
+//    return 0
+//  }
 
-  if (n < 0) {
-    let result = n* -1
-    result = result.toString().split('').reverse().join('')
-    return parseInt(result) * -1
-  } else {
-    let result = n
-    return parseInt(n.toString().split('').reverse().join(''))
-  }
+//  if (n < 0) {
+//    let result = n* -1
+//    result = result.toString().split('').reverse().join('')
+//    return parseInt(result) * -1
+//  } else {
+//    let result = n
+//    return parseInt(n.toString().split('').reverse().join(''))
+//  }
+
+  const reversed = n.toString().split('').reverse().join('')
+  return parseInt(reversed) * Math.sign(n)
+
 
 }
 
