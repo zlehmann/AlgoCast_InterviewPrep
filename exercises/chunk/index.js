7079288d87296af22639974eaaa8f439cf8e6a82
Zachary Lehmann <zlehmann@gmail.com>
2019-10-16 19:02:36 -0400

    alternate solution to chunked.

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -10,16 +10,26 @@
 
 function chunk(array, size) {
   const chunked = []
-  for (let element of array) {
-    const last = chunked[chunked.length-1]
 
-    if (!last || last.length === size) {
-      chunked.push([element])
-    } else {
-      last.push(element)
-    }
+  let index = 0
+
+  while(index<array.length) {
+    chunked.push(array.slice(index, index + size))
+    index = index + size
   }
   return chunked
 }
 
 module.exports = chunk;
+
+//const chunked = []
+//for (let element of array) {
+//  const last = chunked[chunked.length-1]
+//
+//  if (!last || last.length === size) {
+//    chunked.push([element])
+//  } else {
+//    last.push(element)
+//  }
+//}
+//return chunked
