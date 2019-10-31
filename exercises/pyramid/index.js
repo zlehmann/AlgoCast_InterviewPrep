0b3abc824e8b44300571485e12732709c493896e
Zachary Lehmann <zlehmann@gmail.com>
2019-10-31 15:19:05 -0400

    iterative solution for pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,19 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n) {
+  const mid = Math.floor((2*n-1)/2)
+  for (let i=0; i<n; i++) {
+    let level=""
+    for (let j=0; j<(n*2-1); j++){
+      if (mid - i <= j && mid +i >= j) {
+        level += "#"
+      } else {
+        level += " "
+      }
+    }
+    console.log(level)
+  }
+}
 
 module.exports = pyramid;
