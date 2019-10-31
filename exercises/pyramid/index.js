5599e7e68236a1dd6040b454fd4ef33095e88fc5
Zachary Lehmann <zlehmann@gmail.com>
2019-10-31 15:28:30 -0400

    recursive solution for pyramid.

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,19 +14,36 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {
-  const mid = Math.floor((2*n-1)/2)
-  for (let i=0; i<n; i++) {
-    let level=""
-    for (let j=0; j<(n*2-1); j++){
-      if (mid - i <= j && mid +i >= j) {
-        level += "#"
-      } else {
-        level += " "
-      }
-    }
-    console.log(level)
+function pyramid(n, row=0, level= '') {
+  if (row === n) {
+    return
   }
+
+  if (level.length === (n*2-1)) {
+    console.log(level)
+    return pyramid(n, row+1)
+  }
+
+  mid = Math.floor((2*n-1)/2)
+  if (mid - row <= level.length && mid + row >= level.length) {
+    level += "#"
+  } else {
+    level += " "
+  }
+  pyramid(n, row, level)
 }
 
 module.exports = pyramid;
+
+// const mid = Math.floor((2*n-1)/2)
+// for (let i=0; i<n; i++) {
+//   let level=""
+//   for (let j=0; j<(n*2-1); j++){
+//     if (mid - i <= j && mid +i >= j) {
+//       level += "#"
+//     } else {
+//       level += " "
+//     }
+//   }
+//   console.log(level)
+// }
