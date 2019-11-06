fdb0e247fe50108c763ee573df4b10935980c163
Zachary Lehmann <zlehmann@gmail.com>
2019-11-06 14:46:39 -0500

    solves matrix problem

--- exercises/matrix/index.js
+++ exercises/matrix/index.js
@@ -15,6 +15,45 @@
 //     [11, 16, 15, 6],
 //     [10,  9,  8, 7]]
 
-function matrix(n) {}
+function matrix(n) {
+  let results = []
+  for(let i=0; i<n; i++) {
+    let arr = []
+    results.push(arr)
+  }
+  let counter = 1
+  let sRow= 0
+  let eRow= n-1
+  let sCol= 0
+  let eCol= n-1
+
+  while (sCol <= eCol && sRow <= eRow) {
+    //top row
+    for(let i = sCol; i<=eCol; i++) {
+      results[sRow][i] = counter
+      counter ++
+    }
+    sRow ++
+    //right side
+    for(let i = sRow; i<=eRow; i++) {
+      results[i][eCol] = counter
+      counter ++
+    }
+    eCol --
+    //bottom row
+    for(let i = eCol; i>=sCol; i--) {
+      results[eRow][i] = counter
+      counter ++
+    }
+    eRow--
+    //left side
+    for(let i = eRow; i>=sRow; i--){
+      results[i][sCol] = counter
+      counter ++
+    }
+    sCol++
+  }
+  return results
+}
 
 module.exports = matrix;
