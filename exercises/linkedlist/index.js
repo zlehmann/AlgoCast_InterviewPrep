6aa32928c1297851d3960bc81c32d04ad112900f
Zachary Lehmann <zlehmann@gmail.com>
2019-11-20 17:16:48 -0500

    init linkedlist problem

--- exercises/linkedlist/index.js
+++ exercises/linkedlist/index.js
@@ -2,7 +2,12 @@
 // Implement classes Node and Linked Lists
 // See 'directions' document
 
-class Node {}
+class Node {
+  constructor(data, node) {
+    this.data = data
+    this.next = node
+  }
+}
 
 class LinkedList {}
 
