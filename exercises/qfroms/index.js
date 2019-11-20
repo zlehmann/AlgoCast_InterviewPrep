44a7b972f48a7ee2f3390efba745c18d6f44e5f2
Zachary Lehmann <zlehmann@gmail.com>
2019-11-20 17:03:02 -0500

    solves qfroms

--- exercises/qfroms/index.js
+++ exercises/qfroms/index.js
@@ -14,6 +14,41 @@
 
 const Stack = require('./stack');
 
-class Queue {}
+class Queue {
+  constructor() {
+    this.s1 = new Stack()
+    this.s2 = new Stack()
+  }
+
+  add(record) {
+    this.s1.push(record)
+  }
+
+  remove() {
+    while(this.s1.peek()){
+      this.s2.push(this.s1.pop())
+    }
+    const record = this.s2.pop()
+
+    while(this.s2.peek()){
+      this.s1.push(this.s2.pop())
+    }
+
+    return record
+  }
+
+  peek() {
+    while(this.s1.peek()) {
+      this.s2.push(this.s1.pop())
+    }
+
+    const record = this.s2.peek()
+
+    while(this.s2.peek()) {
+      this.s1.push(this.s2.pop())
+    }
+    return record
+  }
+}
 
 module.exports = Queue;
