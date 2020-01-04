2f86f8172792a31058a1f0a4f73f125424b53863
Zachary Lehmann <zlehmann@gmail.com>
2020-01-04 16:08:06 -0500

    adds trigger event handler.

--- exercises/events/index.js
+++ exercises/events/index.js
@@ -19,7 +19,13 @@ class Events {
 
   // Trigger all callbacks associated
   // with a given eventName
-  trigger(eventName) {}
+  trigger(eventName) {
+    if (this.events[eventName]) {
+      for (let cb of this.events[eventName]) {
+        cb();
+      }
+    }
+  }
 
   // Remove all event handlers associated
   // with the given eventName
