02628a46021677611de6560b8c1a95d8f99c7c25
Zachary Lehmann <zlehmann@gmail.com>
2020-01-18 12:42:44 -0500

    solves merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -32,6 +32,15 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+  if (arr.length === 1) {
+    return arr
+  }
+
+  const center = Math.floor(arr.lenth / 2);
+  const left = arr.slice(0, center);
+  const right = arr.slice(center);
+
+  return merge(mergeSort(left), mergeSort(right))
 
 }
 
