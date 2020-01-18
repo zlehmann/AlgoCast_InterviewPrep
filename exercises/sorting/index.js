b41e166f5b07197091d28a25ae0927020ca75517
Zachary Lehmann <zlehmann@gmail.com>
2020-01-18 12:23:32 -0500

    solves merge method for merge sort

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -15,9 +15,9 @@ function bubbleSort(arr) {
 }
 
 function selectionSort(arr) {
-  for (let i = 0; i < arr.length; i++){
+  for (let i = 0; i < arr.length; i++) {
     let indexOfMin = i
-    for (let j = (i+1); j < arr.length; j++){
+    for (let j = (i+1); j < arr.length; j++) {
       if (arr[j] < arr[indexOfMin]) {
         indexOfMin = j
       }
@@ -36,7 +36,15 @@ function mergeSort(arr) {
 }
 
 function merge(left, right) {
-
+  const results = []
+  while (left.length && right.length) {
+    if (left[0] < right[0]) {
+      results.push(left.shift())
+    } else {
+      results.push(right.shift())
+    }
+  }
+  return [...results, ...left, ...right]
 }
 
 module.exports = { bubbleSort, selectionSort, mergeSort, merge };
