3d8acf65dcf734dce2f89b3985e9f990db0ccb9f
Zachary Lehmann <zlehmann@gmail.com>
2020-01-18 11:43:40 -0500

    alternate solution for bubble sort.

--- exercises/sorting/index.js
+++ exercises/sorting/index.js
@@ -3,9 +3,9 @@
 
 function bubbleSort(arr) {
   for(let i=0; i<arr.length; i++) {
-    for(let j=0; j<arr.length-1; j++) {
+    for(let j=0; j<arr.length-i-1; j++) {
       if (arr[j] > arr[j+1]){
-        const temp = arr[j]
+        const lesser = arr[j]
         arr[j] = arr[j+1]
         arr[j+1] = temp
       }
